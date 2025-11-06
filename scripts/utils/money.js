08c469081b9d067fa1ba6f71333d77712864046b
fr0wBtW <georgi_milkov99@abv.bg>
2025-11-07 01:12:52 +0200

    Regenerate HTML when updating delivery option

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -2,4 +2,6 @@ export function formatCurrency(priceCents) {
     return (priceCents / 100).toFixed(2);
 }
 
+
+//Another method to export but we can have only one exported default
 export default formatCurrency; 
