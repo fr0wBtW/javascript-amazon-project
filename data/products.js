0f5532c573e297d61c245794478999c4dfd0e80c
fr0wBtW <georgi_milkov99@abv.bg>
2025-11-20 17:47:52 +0200

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -60,7 +60,30 @@ extraInfoHTML() {
 }
 }
 
-export const products = [
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/* export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -720,8 +743,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
-  if (productDetails.type == 'clothing') {
+  if (productDetails.type === 'clothing') {
     return new Clothing(productDetails);
   }
   return new Product(productDetails);
 });
+*/
